f63c3d9aa289f5104b9401f54e50a9a9e20af874
BayerischeMotorenWerke <syaifulamri199@gmail.com>
2022-09-11 01:01:08 +0700

    sm6150-common: drop more apps
    Signed-off-by: BayerischeMotorenWerke <syaifulamri199@gmail.com>

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,9 +4,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Stk Photos
-LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt
-LOCAL_OVERRIDES_PACKAGES += FM2 Velvet
+LOCAL_OVERRIDES_PACKAGES := Stk Photos DevicePersonalizationPrebuiltPixel2021 GoogleRestorePrebuilt
+LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt ScribePrebuilt TurboPrebuilt WellbeingPrebuilt
+LOCAL_OVERRIDES_PACKAGES += FM2 DevicePolicyPrebuilt AndroidAutoStubPrebuilt PixelLiveWallpaperPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
